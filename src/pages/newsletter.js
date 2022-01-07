ea4f09f08286d2d82b7a71daae4a0573d05de718
Mrinmoy Sarkar <msarkar07@live.in>
2022-01-08 03:13:08 +0530

    feat newletter form base

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -2,7 +2,19 @@ import React from 'react'
 import Layout from '../components/Layout'
 const NewsLetter = () => {
   return (
-    <h4>newsletter</h4>
+  <Layout>
+    <section className='newsletter-page'>
+      <div className='page-center'>
+        <h2>Get all the latest stories to your inbox</h2>
+        <h4> I write to my friends every few weeks</h4>
+        <form  className='contact-form'>
+          <input type='text' className='form-control' name="name" placeholder="Your Name" />
+          <input type='email' className='form-control' name="email" placeholder="Your Email" />
+          <button type='submit' className='btn form-control'>Subscribe</button>
+        </form>
+      </div>
+    </section>
+  </Layout>
   )
 }
 
