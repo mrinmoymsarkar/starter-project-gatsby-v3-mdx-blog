ca93a2fa5ced294cbef497f04a2ec0a3ee8f0044
Mrinmoy Sarkar <msarkar07@live.in>
2022-02-12 20:12:46 +0530

    posts graphql for home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,37 @@ import Hero from '../components/Hero'
 import Layout from '../components/Layout'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
-const IndexPage = () => {
+const IndexPage = ({data}) => {
+  console.log(data)
   return <Layout>
 <Hero showaPerson />
 
   </Layout>
 }
+
+export const query = graphql`
+  {
+    allMdx(sort: {order: ASC, fields: frontmatter___date}, limit: 3) {
+      nodes {
+        id
+        frontmatter {
+          author
+          category
+          readTime
+          slug
+          title
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+            publicURL
+          }
+          date(formatString: "MMMM DD,YYYY")
+        }
+        excerpt
+      }
+    }
+  }
+`
 
 export default IndexPage
